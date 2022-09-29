71ef3e6165f26624d27da5c7b5f837945f440640
Ashraful Islam <ashrafulislam21211@gmail.com>
2022-09-29 14:29:30 +0600

    Title Logo set

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Activity from "./Components/Activity/Activity";
 import ActivityDashBoard from "./Components/ActivityDashBoard/ActivityDashBoard";
 import "./App.css";
+import { FaCentos } from "react-icons/fa";
 function App() {
   const [Activitys, SetActivitys] = useState([]);
   useEffect(() => {
@@ -10,10 +11,28 @@ function App() {
       .then((data) => SetActivitys(data));
   }, []);
   return (
-    <div>
-      {/* <h1 style={{ display: "inline-block", background: "red" }}>
-        Ashraful Islam
-      </h1> */}
+    <div
+      style={{
+        backgroundColor: "#FFFFFF",
+      }}
+    >
+      <div>
+        <FaCentos className="yes"></FaCentos>
+        <h3
+          style={{
+            color: "#363636",
+            textAlign: "center",
+            display: "inline-block",
+            // background: "red",
+            margin: "0",
+            eft: "4px",
+            fontSize: "40px",
+            letterSpacing: "3px",
+          }}
+        >
+          Nur Activity House
+        </h3>
+      </div>
       <div className="ActivityMainBlock">
         <div className="ActivityItemBlock">
           {Activitys.map((activity) => (
